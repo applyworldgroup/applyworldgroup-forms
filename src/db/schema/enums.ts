7445178e2192cd0e6cf99ac1c45a0f05b3fa0d8d
Apply Word Group <amrit@applyworldgroup.com.au>
2024-10-28 15:46:08 +1100

    Update enums.ts

--- src/db/schema/enums.ts
+++ src/db/schema/enums.ts
@@ -1,14 +1,1 @@
-import { pgEnum } from "drizzle-orm/pg-core";
 
-export const enquiryEnum = pgEnum('purpose_of_enquiry_enum', [
-    '482 Visa',
-    '407 Visa',
-    'Student Visa',
-    'Skills Assessment',
-    'Student Enquiry',
-    'Migration Enquiry',
-    '186 Visa',
-    'General Consultation',
-    'PR Pathways',
-    'Other'
-]);
